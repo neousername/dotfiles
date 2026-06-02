5ac20e045afe9de67c4850b14e0c1188e2df3295
neousername <vitaliyovich.oleg@gmail.com>
2026-06-02 10:45:04 +0200

    Changes again

--- .config/scripts/rofi_menu_launcher.sh
+++ .config/scripts/rofi_menu_launcher.sh
@@ -6,11 +6,12 @@ declare -A ICONS=(
     [Email]="$BASE/thunderbird.svg"
     [Telegram]="$BASE/telegram-desktop.svg"
     [Discord]="$BASE/discord.svg"
+    [Signal]="$BASE/signal-desktop.svg"
     [Steam]="$BASE/steam.svg"
     [OBS]="$BASE/obs.svg"
-    [Signal]="$BASE/signal-desktop.svg"
     [LibreOffice]="$BASE/libreoffice7.6-startcenter.svg"
     [Gimp]="$BASE/gimp.svg"
+    [Bitwarden]="$BASE/bitwarden.svg"
 )
 
 declare -A CMDS=(
@@ -18,14 +19,15 @@ declare -A CMDS=(
     [Email]="thunderbird"
     [Telegram]="Telegram"
     [Discord]="discord"
+    [Signal]="signal-desktop"
     [Steam]="steam"
     [OBS]="obs"
-    [Signal]="signal-desktop"
     [LibreOffice]="libreoffice"
     [Gimp]="gimp"
+    [Bitwarden]="bitwarden-desktop"
 )
 
-NAMES=(Firefox Email Telegram Discord Steam OBS Signal LibreOffice Gimp)
+NAMES=(Firefox Email Telegram Discord Signal Steam OBS LibreOffice Gimp Bitwarden)
 
 SELECTED=$(for name in "${NAMES[@]}"; do
     printf "%s\0icon\x1f%s\n" "$name" "${ICONS[$name]}"
