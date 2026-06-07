3229c9f2f9624d876697a59c767206c27bddad96
neousername <vitaliyovich.oleg@gmail.com>
2026-06-07 09:06:46 +0200

    Transparent tmux

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,9 @@ return {
     require("rose-pine").setup({
       variant = "moon", -- auto, main, moon, or dawn
       dark_variant = "moon", -- main, moon, or dawn
+      styles = {
+	transparency = true
+      }
     })
     vim.cmd("colorscheme rose-pine")
   end
