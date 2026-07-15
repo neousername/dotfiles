b64dfdfd5159e01b24d16af2a89b7dd860f3bdf4
neousername <vitaliyovich.oleg@gmail.com>
2026-07-15 10:48:27 +0200

    c

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,12 +6,12 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-tree/nvim-web-devicons", -- optional, but recommended
 	},
-	lazy = false,                -- neo-tree will lazily load itself
+	lazy = false, -- neo-tree will lazily load itself
 	config = function()
 		require("neo-tree").setup({})
 
 		vim.keymap.set("n", "<leader>pv", function()
 			vim.cmd("Neotree source=filesystem reveal=true position=current")
 		end, { desc = "Open Neo-tree fullscreen" })
-	end
+	end,
 }
