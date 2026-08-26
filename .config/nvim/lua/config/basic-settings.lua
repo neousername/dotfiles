9d91dc407750e3471423cd86b3f28446cd5ab738
neousername <vitaliyovich.oleg@gmail.com>
2026-08-26 19:20:22 +0200

    c

--- .config/nvim/lua/config/basic-settings.lua
+++ .config/nvim/lua/config/basic-settings.lua
@@ -1,13 +1,22 @@
 vim.g.mapleader = " "
+
+-- Interface
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
+vim.opt.scrolloff = 100
+
+-- Indentation
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
+-- Status and command lines
 vim.opt.laststatus = 3
 vim.opt.showtabline = 0
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.scrolloff = 100
+vim.opt.cmdheight = 0
+
+-- System integration
 vim.opt.clipboard = "unnamedplus"
-vim.wo.relativenumber = true
-vim.o.cmdheight = 0
-vim.o.autoread = true
+vim.opt.autoread = true
