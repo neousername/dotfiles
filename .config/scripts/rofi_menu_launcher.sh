22d66eac4b3658d38d015c4a69c46914e6409e2a
neousername <vitaliyovich.oleg@gmail.com>
2026-09-04 09:32:54 +0200

    Automatic-Daily-Commit 2026-09-04

--- .config/scripts/rofi_menu_launcher.sh
+++ .config/scripts/rofi_menu_launcher.sh
@@ -3,34 +3,34 @@
 BASE="/usr/share/icons/Papirus-Dark/22x22/apps"
 
 declare -A ICONS=(
-    [Firefox]="$BASE/firefox.svg"
-    [Telegram]="$BASE/telegram.svg"
-    [Signal]="$BASE/signal-desktop.svg"
-    [Discord]="$BASE/discord.svg"
-    [Xournal++]="$BASE/xournalpp.svg"
-    [LibreOffice]="$BASE/libreoffice7.6-startcenter.svg"
-    [OBS]="$BASE/obs.svg"
+  [Firefox]="$BASE/firefox.svg"
+  [Telegram]="$BASE/telegram.svg"
+  [Signal]="$BASE/signal-desktop.svg"
+  [Discord]="$BASE/discord.svg"
+  [Xournal++]="$BASE/xournalpp.svg"
+  [LibreOffice]="$BASE/libreoffice7.6-startcenter.svg"
+  [OBS]="$BASE/obs.svg"
 )
 
 declare -A CMDS=(
-    [Firefox]="firefox"
-    [Telegram]="env QT_QPA_PLATFORM=xcb QT_SCALE_FACTOR=2 XCURSOR_SIZE=24 Telegram"
-    [Signal]="signal-desktop"
-    [Discord]="discord --ozone-platform=wayland"
-    [Xournal++]="xournalpp"
-    [LibreOffice]="libreoffice"
-    [OBS]="obs"
+  [Firefox]="firefox"
+  [Telegram]="env QT_QPA_PLATFORM=xcb QT_SCALE_FACTOR=2 XCURSOR_SIZE=24 Telegram"
+  [Signal]="signal-desktop"
+  [Discord]="discord --ozone-platform=wayland"
+  [Xournal++]="xournalpp"
+  [LibreOffice]="libreoffice"
+  [OBS]="obs"
 )
 
 NAMES=(Firefox Telegram Signal Discord Xournal++ LibreOffice OBS)
 
 SELECTED=$(for name in "${NAMES[@]}"; do
-    printf "%s\0icon\x1f%s\n" "$name" "${ICONS[$name]}"
+  printf "%s\0icon\x1f%s\n" "$name" "${ICONS[$name]}"
 done | rofi -dmenu \
-    -i \
-    -show-icons \
-    -p "Launch:" \
-    -theme "$HOME/.config/rofi/config.rasi")
+  -i \
+  -show-icons \
+  -p "Launch:" \
+  -theme "$HOME/.config/rofi/config.rasi")
 
 [[ -z $SELECTED ]] && exit 0
 
