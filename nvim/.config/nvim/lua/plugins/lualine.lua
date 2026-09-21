ff583b79ed1f6dc921284e72af4a0bc548300af9
neousername <vitaliyovich.oleg@gmail.com>
2026-09-21 16:40:48 +0200

    Commit

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -12,10 +12,7 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch" },
 				lualine_c = { "filename" },
-
-				-- Show the currently connected server and its status
-				lualine_x = { require("opencode").statusline },
-
+				lualine_x = {},
 				lualine_y = { "diagnostics" },
 				lualine_z = { "location", "progress" },
 			},
